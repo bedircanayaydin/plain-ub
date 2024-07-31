c704b09557a8972bac69c47e71b02956e8837fc7
bedircan ayaydın <40080969+bedircanayaydin@users.noreply.github.com>
2024-07-31 04:46:30 +0300

    Update ByBedircan.py

--- app/plugins/tg_tools/ByBedircan.py
+++ app/plugins/tg_tools/ByBedircan.py
@@ -15,7 +15,7 @@ logging.basicConfig(level=logging.INFO)
 
 CHANNEL_ID = [-1001552586568, -1001674072540]
 APK_CHANNEL_ID = {
-    -1001836098073: -1001724179522
+    -1001836098073:
     {-1001836098073
         "id": -1001836098073, 
         "info": 
