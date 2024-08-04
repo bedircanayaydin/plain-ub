447ad7fc0ceb3863ec03a3c1e5039055acfdb917
bedircan ayaydın <40080969+bedircanayaydin@users.noreply.github.com>
2024-08-04 18:28:31 +0300

    Update ByBedircan.py

--- app/plugins/tg_tools/ByBedircan.py
+++ app/plugins/tg_tools/ByBedircan.py
@@ -12,7 +12,7 @@ from app import Message, bot
 
 CHANNEL_ID = [-1001552586568, -1001674072540]
 APK_CHANNEL_ID = {
-    -1001836098073:
+    -1001552586568:
         {
             "id": -1001836098073,
             "info":
@@ -20,7 +20,7 @@ APK_CHANNEL_ID = {
                 "💬 @XposedRepositoryChat \n"+
                 "@Xposedapkrepo"
         },
-    -1001724179522:
+    -1001674072540:
         {
             "id": -1001724179522,
             "info":
